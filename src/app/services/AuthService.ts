a2246dcc6f174598905b5d55cb438d4123fc1149
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-06-17 17:57:54 +0300

    Added registration and logout (generic: void)

--- src/app/services/AuthService.ts
+++ src/app/services/AuthService.ts
@@ -3,8 +3,18 @@ import { AxiosResponse } from "axios"; // container of responce
 import { AuthResponse } from "../models/response/AuthResponse";
 
 export default class AuthService {
-    // in promise we have Checker (interface) of what we will got from server
+    // in promise we have generic which contains interface of what we will got from server
     static async login(email: string, password: string): Promise<AxiosResponse<AuthResponse>> { 
         return $api.post<AuthResponse>('/login', {email, password}) // sending authorization
+    }
+    
+    // in promise we have generic which contains interface of what we will got from server
+    static async registration(email: string, password: string): Promise<AxiosResponse<AuthResponse>> { 
+        return $api.post<AuthResponse>('/registration', {email, password}) // sending authorization
+    }
+    
+    // in promise we have generic which contains interface of what we will got from server
+    static async logout(): Promise<void> { 
+        return $api.post('/logout') // sending authorization
     }
 }
