832d450a31b5b9ad31a1fe07bc3a7b905407f812
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-06-17 16:26:25 +0300

    Added as generic <AuthResponse> we will even able to see what is in response. Like, it we put then after return, we can see it. So we gotta put interaface as generic.

--- src/app/services/AuthService.ts
+++ src/app/services/AuthService.ts
@@ -5,6 +5,6 @@ import { AuthResponse } from "../models/response/AuthResponse";
 export default class AuthService {
     // in promise we have Checker (interface) of what we will got from server
     static async login(email: string, password: string): Promise<AxiosResponse<AuthResponse>> { 
-        return $api.post('/login', {email, password}) // sending authorization
+        return $api.post<AuthResponse>('/login', {email, password}) // sending authorization
     }
 }
