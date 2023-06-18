fa0bcdfdf2eb6f55cc99678823ff7cbc890e51cf
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-06-19 00:27:27 +0300

    added auth window with base validation (BUT GOTTA MAKE IT BETTER!!)

--- src/components/Authorization/Authorization.tsx
+++ src/components/Authorization/Authorization.tsx
@@ -1,15 +1,155 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './Authorization.module.scss';
 
 interface AuthorizationProps {
-    styles: typeof styles;
+  styles: typeof styles;
+}
+
+function Authorization(): JSX.Element {
+  const [isSignIn, setIsSignIn] = useState<boolean>(true);
+  const signInRef = useRef<HTMLSpanElement>(null);
+  const signUpRef = useRef<HTMLSpanElement>(null);
+
+  // data in inputs..
+  const [email,  setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [confirmPassword, setConfirmPassword] = useState<string>('')
+
+  //errors
+  const [isPasswordValid, setIsPasswordValid] = useState<string>('')
+  const [isEmailValid, setIsEmailValid] = useState<string>('')
+  const [isConfirmPasswordValid, setIsConfirmPasswordValid] = useState<string>('')
+
+
+
+  const handleSigning = (action: boolean) => {
+    setIsSignIn(action);
+  }; // jut switchin sign in/up
+  
+  const handleSignUpSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const timeout: number = 9000;
+  
+    // validation 
+    if (!/^[a-zA-Z]+$/.test(password) || password.length < 8) {
+      setIsPasswordValid('The password must contain only Latin characters and be at least 8 characters long!');
+  
+      setTimeout(() => {
+        setIsPasswordValid('');
+      }, timeout);
+      return;
+    }
+    // validation
+    if (password !== confirmPassword) {
+      setIsPasswordValid('You have different passwords with some reason!');
+  
+      setTimeout(() => {
+        setIsPasswordValid('');
+      }, timeout);
+      return;
+    }
+
+    // Check email format
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+      setIsEmailValid('Write email correctly!');
+
+      setTimeout(() => {
+        setIsEmailValid('');
+      }, timeout);
+      return;
+    }
+
+    
+    console.log('Everything working in sign up yet.');
+    
+
+
+    
   }
   
-function Authorization(): JSX.Element {
+  const handleSignInSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const timeout: number = 9000;
+  
+    // validation 
+    if (!/^[a-zA-Z]+$/.test(password) || password.length < 8) {
+      setIsPasswordValid('The password must contain only Latin characters and be at least 8 characters long!');
+  
+      setTimeout(() => {
+        setIsPasswordValid('');
+      }, timeout);
+      return;
+    }
+
+
+    // Check email format
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+      setIsEmailValid('Write email correctly!');
+
+      setTimeout(() => {
+        setIsEmailValid('');
+      }, timeout);
+      return;
+    }
+
+    
+    console.log('Everything working in sign IN yet.');
+
+  }
+
+
+
   return (
-    <div className="Authorization">
+    <div className={styles.Authorization}>
+      <div className={styles.title}>
+        <span
+          className={isSignIn ? styles.underline : ''}
+          ref={signInRef}
+          onClick={() => handleSigning(true)}
+        >
+          Sign in
+        </span>
+        <span> / </span>
+        <span
+          className={!isSignIn ? styles.underline : ''}
+          ref={signUpRef}
+          onClick={() => handleSigning(false)}
+        >
+          Sign up
+        </span>
+      </div>
+
+      {isSignIn && (
+        <form onSubmit={(e) => handleSignInSubmit(e)}>
+          <div className={styles.inputs}>
+            <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <div className={styles.continue}>
+            <button>continue</button>
+          </div>
+        </form>
+      )}
+
+      {!isSignIn && (
+        <form onSubmit={(e) => handleSignUpSubmit(e)}>
+          <div className={styles.inputs}>
+            <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <input placeholder="Confirm pass" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+          </div>
+          <div className={styles.continue}>
+            <button>continue</button>
+          </div>
+        </form>
+      )}
+
+      {isPasswordValid && <div className={styles.error}>{isPasswordValid}</div>}
+      {isEmailValid && <div className={styles.error}>{isEmailValid}</div>}
     </div>
-)
+  );
 }
 
 export default Authorization;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import styles from './Header.module.scss';
 import search_icon from '../../assets/images/auth_icon.png';
 import { Link } from 'react-router-dom';
+import Authorization from '../Authorization/Authorization.tsx';
+import { useState } from 'react';
 
 const Header = () => {
+  const [authWindowOpened, setAuthWindowOpened] = useState(false);
   return (
-    <div className={styles.wrapper}>
-      <Link to="/" className={styles.header}>
-        {'<Codebar />'}
-      </Link>
-      <img className={styles.account_icon} src={search_icon} alt="" />
-    </div>
+    <>
+      {!authWindowOpened && <Authorization />}
+
+      <div className={styles.wrapper}>
+        <Link to="/" className={styles.header}>
+          {'<Codebar />'}
+        </Link>
+        <img
+          onClick={() => setAuthWindowOpened(!authWindowOpened)}
+          className={styles.account_icon}
+          src={search_icon}
+          alt=""
+        />
+      </div>
+    </>
   );
 };
 
