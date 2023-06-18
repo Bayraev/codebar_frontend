f889bedfefce273e34e06ec9c4987fb3eead894d
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-06-18 17:39:39 +0300

    add a bit comments

--- src/app/service/AuthService.ts
+++ src/app/service/AuthService.ts
@@ -7,6 +7,8 @@ export default class AuthService {
     // in promise we have generic which contains interface of what we will got from server
     static async login(email: string, password: string): Promise<AxiosResponse<AuthResponse>> { 
         return $api.post<AuthResponse>('/login', {email, password}) // sending authorization
+        //* We need this generic here to see what it will contain for exmp here:
+        //* .then(res => res.data.)
     }
     
     // in promise we have generic which contains interface of what we will got from server
