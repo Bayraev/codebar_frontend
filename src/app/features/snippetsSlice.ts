cdd7e9c1ad9536948d9b9df164871be1babe1843
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-06-20 21:09:15 +0300

    Implemented simple adding to store new snippet. Need to upd ofc.

---
+++ src/app/features/snippetsSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { ISnippet } from "../models/ISnippet"
+
+interface SliceState {
+    snippets: ISnippet[];
+}
+const initialState: SliceState = {
+    snippets: [{
+    "ownerId": null,
+    "title": "text",
+    "snippet": "<h> Hello World!</h>",
+    "description": "Little description",
+    "tags": ["Js", "HTML", "tag"],
+    "hidden": false
+}]
+}
+
+const snippetsSlice = createSlice({
+    name: 'snippets',
+    initialState,
+    reducers: {
+        addSnippet: (state, action) => {
+            const snippet = action.payload // obj
+            state.snippets.push(snippet) 
+        }
+    }
+})
---
+++ src/app/models/ISnippet.ts
@@ -0,0 +1,16 @@
+interface ITags {
+    [index: number]: string; // its literally an onject, indexed by numbers. 
+}
+interface IImages {
+    [index: number]: string; // its literally an onject, indexed by numbers. 
+}
+
+export interface ISnippet{
+    ownerId?: string;
+    title: string;
+    snippet: string;
+    description: string
+    hidden?: boolean;
+    tags?: ITags[];
+    image?: IImages[];
+}
