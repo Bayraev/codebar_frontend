5f3ac58135c03248250cdfad238728d878e9282f
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-07-02 17:43:23 +0300

    Logout working. But NEXT step fix isAuthWindowOpened

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import search_icon from '../../assets/images/auth_icon.png';
+import login_icon from '../../assets/images/auth_icon.png';
+import logout_icon from '../../assets/images/logout_icon.png';
 import { Link } from 'react-router-dom';
 import Authorization from '../Authorization/Authorization.tsx';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../app/features/AuthSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.authorization.isAuth);
   const [authWindowOpened, setAuthWindowOpened] = useState(false);
+
+  const handleLogout = () => {
+    alert('Logout..');
+    setAuthWindowOpened(!authWindowOpened);
+    dispatch(logout());
+  };
+
   return (
     <>
       {authWindowOpened && <Authorization />}
@@ -15,12 +27,16 @@ const Header = () => {
         <Link to="/" className={styles.header}>
           {'<Codebar />'}
         </Link>
-        <img
-          onClick={() => setAuthWindowOpened(!authWindowOpened)}
-          className={styles.account_icon}
-          src={search_icon}
-          alt=""
-        />
+
+        {isAuth ? (
+          <img onClick={() => handleLogout()} className={styles.account_icon} src={logout_icon} />
+        ) : (
+          <img
+            onClick={() => setAuthWindowOpened(!authWindowOpened)}
+            className={styles.account_icon}
+            src={login_icon}
+          />
+        )}
       </div>
     </>
   );
