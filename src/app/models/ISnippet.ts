84fd1fa16790293993a622487bd7e4aebc69a788
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-08-07 14:01:06 +0300

    I have created interaface for snippet object and rename snippets array interface

--- src/app/models/ISnippet.ts
+++ src/app/models/ISnippet.ts
@@ -5,7 +5,20 @@ export interface IImages {
     [index: number]: string; // its literally an onject, indexed by numbers. 
 }
 
-export interface ISnippet{
+// for obj
+export interface ISnippetObj {
+    ownerId?: string;
+    uniqId: string;
+    title: string;
+    snippet: string;
+    description: string
+    hidden?: boolean;
+    tags?: ITags[];
+    image?: IImages[];
+}
+
+// for arr
+export interface ISnippetsArr{
     ownerId?: string;
     uniqId: string;
     title: string;
