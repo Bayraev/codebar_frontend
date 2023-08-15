46610fb988e61fd0cabec6c20ddbd11f61855e50
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-08-15 14:33:56 +0300

    Implemented asyncNewSnippet

--- src/app/service/SnippetsService.ts
+++ src/app/service/SnippetsService.ts
@@ -9,7 +9,8 @@ export default class SnippetsService {
     static async asyncGetSnippets(ownerId: string): Promise<AxiosResponse<ISnippets>> {
         return $api.get<ISnippets>(`/snippets/`+ownerId)
     }
-    // static async asyncNewSnippet(snippet: ISnippetObj): Promise<AxiosResponse<ISnippets>> {
-    //     return $api.post<ISnippets>('/new_snippet')
-    // }
+    // next static sents request to create new snippet in DB, sending snippet with interface ISnippetObj
+    static async asyncNewSnippet(snippet: ISnippetObj): Promise<AxiosResponse<ISnippets>> {
+        return $api.post<ISnippets>('/new_snippet', snippet)
+    }
 }
